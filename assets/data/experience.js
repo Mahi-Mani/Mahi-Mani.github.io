e7bcf1aee112ada56f69f37337e7b1ee87f578b8
Mahi Mani <m.mahishamani@gmail.com>
2020-12-29 14:43:23 -0800

    Add work experience to script file

---
+++ assets/data/experience.js
@@ -0,0 +1,33 @@
+const workExperience = [
+  {
+    "companyName": "Trilogy Education Services",
+    "startDate": "November 2019",
+    "endDate": "Present",
+    "role": "Full Stack Teaching Assistant",
+    "responsibilities": ["Assisted a total of 30 students", "Analyzed code and helped students fix issues with their projects",
+                        "Helped the instructor with curriculum development", "Responsible for improving technical skills of students",
+                      "Mentored students in their projects and evaluated their assignments"]
+  },
+  {
+    "companyName": "Ziggurat Technologies",
+    "startDate": "September 2020",
+    "endDate": "November 2020",
+    "role": "Front End Developer - Intern",
+    "responsibilities": ["Responsible for building intuitive UIs for both iOS and Android platforms", "Worked on transforming native iOS apps to React native",
+                        "Leveraged Redux framework for transient app state", "Handled RESTful APIs which support JSON to access backend data",
+                        "Implemented component life cycle architecture, store actions to handle real time data from third party APIs",
+                        "Diagnosed and fixed bugs/performance bottlenecks"]
+  },
+  {
+    "companyName": "Cognizant Technologies Solutions",
+    "startDate": "June 2016",
+    "endDate": "July 2018",
+    "role": "Programmer Analyst",
+    "responsibilities": ["Engaged in L2 / L3 technical support regarding application issues and serviced multiple standard requests from external business partners and customers",
+                        "Developed script to automate job flows that reduced manual effort by 70%",
+                        "Responsible for observing 24/7 application availability through a structured monitoring framework",
+                        "Specialized in debugging and performing regression testing on code changes",
+                        "Accomplished end-to-end setup for onboarding new customer using SFTP protocol",
+                        "Managed application roll-out activities such as preparation of weekly status reports and timesheet management"]
+  }
+]
