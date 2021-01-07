ee57f9b52f482eb47ad0a53dc52964fce0a2ed20
Mahisha Gunasekaran <m.mahishamani@gmail.com>
2021-01-07 11:07:18 -0800

    Revert back breakpoint changes

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -11,14 +11,12 @@ $(document).ready(function () {
   var isExpandedContact;
   var width;
   var height;
-  var projectsWidth;
-  var breakPoint;
+  // var projectsWidth;
+  // var breakPoint;
 
   $(window).resize(function () {
-    console.log(wrapper.width());
     width = wrapper.width();
-    projectsWidth = $("#projects").width();
-    console.log(projectsWidth);
+    // projectsWidth = $("#projects").width(); 
   });
 
   $(window).scroll(function () {
@@ -140,24 +138,28 @@ $(document).ready(function () {
     }
   }
 
-  if (projectsWidth < "576") {
-    breakPoint = 2;
-  } else {
-    breakPoint = 3;
-  }
   // Hook projects dom
   for (var i = 0; i < projects.length; i++) {
     height = Math.floor(width / projects[i].ratio);
-    if (i % breakPoint === 0) {
+    // if (projectsWidth < 576) {
+    //   console.log("Inside if loop");
+    //   breakPoint = 2;
+    // } else {
+    //   console.log("Inside else loop");
+    //   breakPoint = 3;
+    // }
+// console.log(breakPoint);
+// console.log(width)
+    if (i % 3 === 0) {
       var rowDiv = $("<div>");
       addClassValue(rowDiv, "row");
       var col1Div = $("<div>");
       addClassValue(col1Div, "col-1 col-sm-1 col-md-1 col-lg-1 col-xl-1");
-      var col2Div = $("<div>");
-      if (breakPoint === 3)
-        addClassValue(col2Div, "col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3");
-      else
-        addClassValue(col2Div, "col-5 col-sm-5 col-md-5 col-lg-5 col-xl-5");
+      // var col2Div = $("<div>");
+      // if (i === 3)
+        // addClassValue(col2Div, "col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3");
+      // else
+      //   addClassValue(col2Div, "col-5 col-sm-5 col-md-5 col-lg-5 col-xl-5");
       addAttribute(col2Div, "data-aos", "flip-up");
       addAttribute(col2Div, "data-aos-duration", "1000");
       var wrapper = $("<div>");
@@ -187,13 +189,13 @@ $(document).ready(function () {
       appendElement(rowDiv, col2Div);
     } else {
       var col3Div = $("<div>");
-      if (breakPoint === 3)
-        col3Div.addClass("col-5 col-sm-3 col-md-3 col-lg-3 col-xl-3");
-      else
-        col3Div.addClass("col-5 col-sm-5 col-md-5 col-lg-5 col-xl-5");
+      // if (i === 3)
+        col3Div.addClass("col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3");
+      // else
+      //   col3Div.addClass("col-5 col-sm-5 col-md-5 col-lg-5 col-xl-5");
       addAttribute(col3Div, "data-aos", "flip-up");
       addAttribute(col3Div, "data-aos-duration", "1000");
-      if (i % 3 === 1 && breakPoint === 3) {
+      if (i % 3 === 1) {
         var col4Div = $("<div>");
         col4Div.addClass("col-1 col-sm-1 col-md-1 col-lg-1 col-xl-1");
       }
@@ -221,8 +223,8 @@ $(document).ready(function () {
       appendElement(wrapper, overlayDiv);
       appendElement(col3Div, wrapper);
       appendElement(rowDiv, col3Div);
-      if (breakPoint === 3)
-        appendElement(rowDiv, col4Div);
+      // if (breakPoint === 3)
+      //   appendElement(rowDiv, col4Div);
     }
     $("#projects").append(rowDiv);
   }
@@ -264,6 +266,9 @@ $(document).ready(function () {
 
   // Function to append an element
   function appendElement(parent, child) {
+    console.log("Append Element");
+    console.log(parent);
+    console.log(child);
     parent.append(child);
   }
 
@@ -302,7 +307,6 @@ $(document).ready(function () {
   // Generate modal
   $(".learnMore").on("click", function (event) {
     event.preventDefault();
-    console.log("Inside learn more button click funtion");
     var id = $(this).data("id");
     var name;
     var desc;
