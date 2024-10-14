1444eb6596d6eb872fc9bd638a7edf8cbbacec0c
Mahi-Mani <m.mahishamani@gmail.com>
2024-10-13 22:14:53 -0700

    Add work experience

--- assets/data/experience.js
+++ assets/data/experience.js
@@ -3,10 +3,11 @@ const workExperience = [
     "companyName": "Santa Clara University",
     "startDate": "February 2024",
     "endDate": "Present",
-    "role": "Software Engineer Intern/Research Assistant",
+    "role": "Software Engineer Intern/ Research Assistant",
     "location": "Santa Clara",
-    "responsibilities": ["Analyzed large text reflections from product users using NLP techniques.", "Designed and developed data visualizations using Javascript.",
-                        "Developed backend functionality using Django framework including REST API creation.", "Managed and maintained database operations, performance enhancement."]
+    "responsibilities": ["Analyze user feedback via NLP for improved insight accuracy and data-driven decisions.", "Develop 20+ custom REST APIs using Django framework to support various business logic and ensuring seamless frontend integration.",
+                        "Engineer interactive data visualizations using JavaScript frameworks, d3.js to deliver intuitive and responsive user interfaces, reducing time spent on report by 50%.", "Resolve complex multi-label text classification problems by integrating machine learning models into web frameworks to enhance application functionality.",
+                      "Enhance model prediction accuracy through continuous refinement and data-driven optimizations."]
   },
   {
     "companyName": "Trilogy Education Services",
@@ -14,9 +15,9 @@ const workExperience = [
     "endDate": "July 2021",
     "role": "Full Stack Teaching Assistant",
     "location": "San Francisco",
-    "responsibilities": ["Assisted a total of 120 students", "Analyzed code and helped students fix issues with their projects",
-                        "Helped the instructor with curriculum development", "Responsible for improving technical skills of students",
-                      "Mentored students in their projects and evaluated their assignments"]
+    "responsibilities": ["Led approximately 120 students in journey to becoming web developers.", "Collaborated with students on diverse projects, ensuring adherence to high coding standards through code review and feedback sessions, and fostering effective teamwork by adapting to evolving project needs in a dynamic environment.",
+                        "Mentored 12 teams per cohort, with 10 teams showcasing projects at public events.", "Identified and addressed common issues, utilizing strong communication skills to provide clear guidance, resulting in a notable increase in average student grades by 30%.",
+                      "Trained students and provided detailed guidance on code optimization and debugging, resulting in a 50% boost in project completion rates."]
   },
   {
     "companyName": "Ziggurat Technologies",
@@ -24,10 +25,8 @@ const workExperience = [
     "endDate": "November 2020",
     "role": "Front End Developer - Intern",
     "location": "San Jose",
-    "responsibilities": ["Responsible for building intuitive UIs for both iOS and Android platforms", "Worked on transforming native iOS apps to React native",
-                        "Leveraged Redux framework for transient app state", "Handled RESTful APIs which support JSON to access backend data",
-                        "Implemented component life cycle architecture, store actions to handle real time data from third party APIs",
-                        "Diagnosed and fixed bugs/performance bottlenecks"]
+    "responsibilities": ["Maximized the Redux framework to manage app state in a large-scale React project, working with cross-functional teams of 8+ members including UI/UX designers and backend developers to deliver a cohesive and efficient application.",
+                        "Diagnosed and fixed around 30+ bugs through thorough debugging, unit and integration testing, resulting in improved application efficiency."]
   },
   {
     "companyName": "Cognizant Technologies Solutions",
@@ -35,11 +34,7 @@ const workExperience = [
     "endDate": "July 2018",
     "role": "Programmer Analyst",
     "location": "India",
-    "responsibilities": ["Engaged in L2 / L3 technical support regarding application issues and serviced multiple standard requests from external business partners and customers",
-                        "Developed script to automate job flows that reduced manual effort by 70%",
-                        "Responsible for observing 24/7 application availability through a structured monitoring framework",
-                        "Specialized in debugging and performing regression testing on code changes",
-                        "Accomplished end-to-end setup for onboarding new customer using SFTP protocol",
-                        "Managed application roll-out activities such as preparation of weekly status reports and timesheet management"]
+    "responsibilities": ["Engaged in L2 / L3 technical support, applying diagnostic tools and troubleshooting techniques to resolve technical issues, addressed over 50+ high priority problems.",
+                        "Devised script to automate job flows, reducing manual effort by 70%."]
   }
 ]
