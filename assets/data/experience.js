f4c9a37753077b03671f6baadba04f36183d26f9
Mahi-Mani <m.mahishamani@gmail.com>
2025-02-07 16:11:21 -0800

    Edit roles and responsibilities of intern

--- assets/data/experience.js
+++ assets/data/experience.js
@@ -6,8 +6,8 @@ const workExperience = [
     "role": "Software Engineer Intern/ Research Assistant",
     "location": "Santa Clara",
     "responsibilities": ["Perform sentiment analysis on user feedback entries using the NLTK library, identifying insights that increased user engagement with sustainability features by 10%.", "Classify user feedback into multiple labels using Binary Relevance and Logistic Regression, achieving an 18% accuracy improvement through hyperparameter tuning and addressing class imbalances with SMOTE.",
-                        "Optimize model performance by extracting and prioritizing the top 15% of impactful features using feature importance analysis, resulting in faster predictions.", "Engineer a Django-based full-stack web application integrating the best-performing model, enabling real-time prediction and classification of new user feedback with 88% accuracy.",
-                      "Design and develop interactive frontend visualizations using JavaScript and Chart.js to simplify feedback analysis, introducing label-specific text highlighting and distribution charts, which streamlined complex data review processes."]
+      "Optimize model performance by extracting and prioritizing the top 15% of impactful features using feature importance analysis, resulting in faster predictions.", "Engineer a Django-based full-stack web application integrating the best-performing model, enabling real-time prediction and classification of new user feedback with 88% accuracy.",
+      "Design and develop interactive frontend visualizations using JavaScript and Chart.js to simplify feedback analysis, introducing label-specific text highlighting and distribution charts, which streamlined complex data review processes."]
   },
   {
     "companyName": "Trilogy Education Services",
@@ -16,8 +16,8 @@ const workExperience = [
     "role": "Full Stack Teaching Assistant",
     "location": "San Francisco",
     "responsibilities": ["Led approximately 120 students in journey to becoming web developers.", "Collaborated with students on diverse projects, ensuring adherence to high coding standards through code review and feedback sessions, and fostering effective teamwork by adapting to evolving project needs in a dynamic environment.",
-                        "Mentored 12 teams per cohort, with 10 teams showcasing projects at public events.", "Identified and addressed common issues, utilizing strong communication skills to provide clear guidance, resulting in a notable increase in average student grades by 30%.",
-                      "Trained students and provided detailed guidance on code optimization and debugging, resulting in a 50% boost in project completion rates."]
+      "Mentored 12 teams per cohort, with 10 teams showcasing projects at public events.", "Identified and addressed common issues, utilizing strong communication skills to provide clear guidance, resulting in a notable increase in average student grades by 30%.",
+      "Trained students and provided detailed guidance on code optimization and debugging, resulting in a 50% boost in project completion rates."]
   },
   {
     "companyName": "Ziggurat Technologies",
@@ -25,8 +25,10 @@ const workExperience = [
     "endDate": "November 2020",
     "role": "Front End Developer - Intern",
     "location": "San Jose",
-    "responsibilities": ["Maximized the Redux framework to manage app state in a large-scale React project, working with cross-functional teams of 8+ members including UI/UX designers and backend developers to deliver a cohesive and efficient application.",
-                        "Diagnosed and fixed around 30+ bugs through thorough debugging, unit and integration testing, resulting in improved application efficiency."]
+    "responsibilities": ["Developed UI for the About and Payment pages of a mobile app leveraging React Native, elevating user experience and reducing load times by 20% through efficient state management with Redux",
+      "Conducted code reviews and resolved critical bugs through systematic debugging, unit and integration testing, to improve app performance and diminishing error rates by 10%."
+      , "Collaborated closely with UX/UI designers using Figma to build intuitive and responsive mobile app pages, leading to a 25% increase in user satisfaction based on user feedback surveys."
+    ]
   },
   {
     "companyName": "Cognizant Technologies Solutions",
@@ -35,6 +37,6 @@ const workExperience = [
     "role": "Programmer Analyst",
     "location": "India",
     "responsibilities": ["Engaged in L2 / L3 technical support, applying diagnostic tools and troubleshooting techniques to resolve technical issues, addressed over 50+ high priority problems.",
-                        "Devised script to automate job flows, reducing manual effort by 70%."]
+      "Devised script to automate job flows, reducing manual effort by 70%."]
   }
 ]
